4e4aef46c33e07f6ef77b22b7af7833ab16367c2
Mo <ademoyosoreoluwa@hotmail.com>
2024-08-28 20:37:00 +0100

    This will allow you to view vidoes or movies while working without anyone knowing that you are watching something

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log('Testing');
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select Media Stream, then play
+
+async function selectMediaStream(){
+    try{
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.onplay();
+        }
+    } catch (error) {
+        console.log('Whops, error here:', error);
+
+    }
+}
+
+button.addEventListener('click', async() => {
+    // Disable Button
+    button.disabled = true;
+    //  Start Picture in Picture 
+    await videoElement.requestPictureInPicture();
+    // Reset Button
+    button.disabled = false;
+})
+
+
+// On Load
+selectMediaStream();
